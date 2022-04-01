6de70a0c77d90a4a991848628f84217e63a9da6d
SIVAPRAKASHK3 <101009349+SIVAPRAKASHK3@users.noreply.github.com>
2022-04-01 17:48:34 +0530

    Add files via upload

---
+++ 2_Implementation/name.h
@@ -0,0 +1,14 @@
+char str11[10][10]={
+					"apple",
+					"zucchini",
+					"wolfbery",
+					"voavanga",
+					"rambutan",
+					"quince",
+					"peach",
+					"pear",
+					"olive",
+					"Nectarine"
+					};
+					
+					char dup[10][10];
---
+++ 2_Implementation/students_remark.c
@@ -0,0 +1,39 @@
+#include<string.h>
+#include<stdio.h>
+char str1[6]="12345";
+int passcheck(char x[]){
+	if(strcmp(x,str1)==0)
+	return 1;
+	
+	return 0;
+}
+//void remark(){
+//	printf("*To read remarks type 1 \n\t*To report type 2 \n");
+//	int itr;
+//		char ch=' ';
+//		char ch1=' ';
+//	scanf("%d",&itr);
+//	switch(itr){
+//		case 1:
+//			display();
+//		        printf("\nNavigate to menu type ? y/n ");
+//		        scanf("%c",&ch1);
+//		        scanf("%c",&ch);
+//		       // getch();
+//		        if(ch=='y')
+//		            menu();
+//		        else
+//		            printf("Thank you");
+//					break;
+//		case 2:
+//			sample();
+//			break;
+//	}}
+//
+//void sample(){
+////	char ch=' ';
+////	char ch1=' ';
+//	printf("FORMATE -> FIRST ROLL NUMBER FOLLOWED BY - THEN TYPE YOUR MESSEGE AT END CLOSE WITH | AT THE END OF REMARK CLOSE WITH @ \nEXAMPLE :1-IN ATTENTIV|2.TALKATTIVE@\n");
+//scanf("%100s",chat);
+//cpy();
+//}
