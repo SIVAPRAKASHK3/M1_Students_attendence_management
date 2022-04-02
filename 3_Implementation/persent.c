24aff05c420c0258e910bab0004bf38e93798e1f
SIVAPRAKASHK3 <101009349+SIVAPRAKASHK3@users.noreply.github.com>
2022-04-02 12:46:07 +0530

    Add files via upload

---
+++ 3_Implementation/persent.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+extern void add();
+extern int attendence_eligibility(int ro_no,int num_days);
+int pers(int roll,int numdays){
+    add();
+if(attendence_eligibility(roll,numdays)==1)
+return 1;
+else
+return 0;
+}
+// int main(){
+//     printf("%d",pers(1,2));
+// }
